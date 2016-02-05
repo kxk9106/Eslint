340f8d7750c86014d73f3fbc69bb271c0d32d7ec
Kristen Kanekuni <kxk9106@OH1370-17.main.ad.rit.edu>
2016-02-05 09:44:53 -0500

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,2
+        ],
+        "no-console": [0],
+        "no-undef":[0],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "semi": [
+            2,
+            "never"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "browser": false
+    },
+    "extends": "eslint:recommended"
+};
